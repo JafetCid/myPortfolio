10437f51ad8f4de1138886d6e98e43f156b242bf
JafetCid <jafetcid9@gmail.com>
2025-07-13 21:47:10 -0600

    portfolio v4

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: isProd ? '/mi-repo' : '',
+  assetPrefix: isProd ? '/mi-repo/' : '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
