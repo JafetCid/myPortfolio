035b6e6f8cd900440682f63ce3e40adf44dd9439
JafetCid <jafetcid9@gmail.com>
2025-11-04 12:32:25 -0600

    navbar corrections

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,16 +10,15 @@ export default function Navbar() {
             initial={{ opacity: 0, y: -10 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, ease: "easeIn" }}
-            className="flex w-full py-5 px-5 justify-between text-black bg-white md:!fixed md:px-16 z-10">
-            <div className="flex items-center">
-                {/* <i className="bi bi-code-slash text-xl lg:text-2xl"></i> */}
+            className="fixed w-full py-5 px-5 text-black bg-white md:px-16 z-10">
+            <div className="flex justify-between">
                 <h1 className="font-bold text-xl lg:text-2xl">Jafet Cid</h1>
-            </div>
-            <div className="hidden items-center gap-5 md:flex">
-                <Link href="#home" className="relative hover:after:w-full after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:bg-black after:transition-all after:duration-300">Inicio</Link>
-                <Link href="#aboutMe" className="relative hover:after:w-full after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:bg-black after:transition-all after:duration-300">Sobre mí</Link>
-                <Link href="#project" className="relative hover:after:w-full after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:bg-black after:transition-all after:duration-300">Proyectos</Link>
-                <Link href="#contact" className="relative hover:after:w-full after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:bg-black after:transition-all after:duration-300">Contacto</Link>
+                <div className="hidden items-center gap-5 md:flex">
+                    <Link href="#home" className="relative hover:after:w-full after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:bg-black after:transition-all after:duration-300">Inicio</Link>
+                    <Link href="#aboutMe" className="relative hover:after:w-full after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:bg-black after:transition-all after:duration-300">Sobre mí</Link>
+                    <Link href="#project" className="relative hover:after:w-full after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:bg-black after:transition-all after:duration-300">Proyectos</Link>
+                    <Link href="#contact" className="relative hover:after:w-full after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:bg-black after:transition-all after:duration-300">Contacto</Link>
+                </div>
             </div>
         </motion.nav>
     )
